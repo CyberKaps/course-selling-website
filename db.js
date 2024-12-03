98123d66bae0910dab00b628768ab8f50e28e575
Kalpesh Borde <bordekalpesh@gmail.com>
2024-12-03 13:21:22 +0530

    added the schema

--- db.js
+++ db.js
@@ -2,24 +2,24 @@ const { Schema, default: mongoose } = require("mongoose");
 console.log("connected to")
 mongoose.connect("mongodb+srv://cyberkaps:kalpesh@cluster0.dyv70o5.mongodb.net/course-selling-web")
 const user = require("./routes/user");
-const Schema = mongoose.Schema;
+
 const ObjectId = mongoose.Types.ObjectId;
 
-const userSchema = Schema({
+const userSchema = new Schema({
     email: { type: String, unique: true },
     password: String,
     firstName: String,
     lastName: String
 });
 
-const adminSchema = Schema({
+const adminSchema = new Schema({
     email: { type: String, unique: true },
     password: String,
     firstName: String,
     lastName: String
 });
 
-const courseSchema = Schema({
+const courseSchema = new Schema({
     title: String,
     description: String,
     price: Number,
@@ -27,15 +27,15 @@ const courseSchema = Schema({
     creatorId: ObjectId
 });
 
-const purchaseSchema = Schema({
+const purchaseSchema = new Schema({
     userId: ObjectId,
     courseId: ObjectId
 });
 
-const userModel = mongoose.Model("users", userSchema);
-const adminModel = mongoose.Model("admin", adminSchema);
-const courseModel = mongoose.Model("course", courseSchema);
-const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+const userModel = mongoose.model("users", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
 
 module.exports = {
     userModel: userModel,
