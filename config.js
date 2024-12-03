203829a01db0cde2f45e8917bfb1a8864dd73606
Kalpesh Borde <bordekalpesh@gmail.com>
2024-12-03 23:13:43 +0530

    added .env.example

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const JWT_USER_PASSWORD = "kaps"
-const JWT_ADMIN_PASSWORD = "admin"
+
+
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 
 module.exports = {
     JWT_ADMIN_PASSWORD,
